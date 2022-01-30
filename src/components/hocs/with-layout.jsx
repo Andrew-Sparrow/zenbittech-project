5b072d73abef443a6885362867c4a4783a43b0e8
Andrew <8911102@gmail.com>
2022-01-30 22:17:41 +0500

    footer for wrapper was added

--- src/components/hocs/with-layout.jsx
+++ src/components/hocs/with-layout.jsx
@@ -2,19 +2,26 @@ import Layout from '../layout/layout';
 import styled from 'styled-components';
 
 function withLayout(Component, className) {
-  const LayoutStyle = styled.div`
-    width: 1440px;
-    margin-left: auto;
-    margin-right: auto;
+  const WrapperStyle = styled.div`
+    .wrapper-footer {
+      position: relative;
+      background-color: #FAFAFA;
+      min-height: 200px;
+      border-top: 1px solid #D8D8D8;
+      margin-top: -201px;
+      z-index: 100;
+      background-color: #FAD34F;
+    }
   `;
 
   return function wrapper(props) {
     return (
-      <LayoutStyle className={className}>
+      <WrapperStyle className={className}>
         <Layout>
           <Component {...props} />
         </Layout>
-      </LayoutStyle>
+        <div className="wrapper-footer"></div>
+      </WrapperStyle>
     );
   };
 }
--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,14 +1,24 @@
 import PropTypes from 'prop-types';
 import Footer from '../footer/footer';
+import styled from 'styled-components';
+
 
 function Layout(props) {
   const {children} = props;
 
+  const LayoutStyle = styled.div`
+    width: 1440px;
+    margin-left: auto;
+    margin-right: auto;
+    z-index: 200;
+    position: relative;
+  `;
+
   return (
-    <div className="layout">
+    <LayoutStyle className="layout">
       {children}
-      <Footer/>
-    </ div>
+      <Footer />
+    </LayoutStyle>
   );
 }
 
